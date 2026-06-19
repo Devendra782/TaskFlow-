2091fdd0d2c23fd26435ec59940253010725f297
Devendra Dangi <devendradangi824@gmail.com>
2026-06-19 16:12:14 +0530

    Fixed production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://taskflow-backend-d2yq.onrender.com/",
+  baseURL: "https://taskflow-backend-d2yq.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
